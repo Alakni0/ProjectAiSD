429e2ee7d7c37bebdc41f5edfc20db27d4f868ce
Alakni0 <ala.knigawka@gmail.com>
2026-01-09 20:38:00 +0100

    takie tam

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ def algorytm_wyszukiwania(tekst,wzorzec):
     tekstDlugosc=len(tekst)
     wzorzecDlugosc=len(wzorzec)
 
-    #if wzorzecDlugosc>tekstDlugosc or wzorzecDlugosc==0:
-     #   return wynik
+    if wzorzecDlugosc>tekstDlugosc or wzorzecDlugosc==0:
+        print("Brak mozliwosci wyszukiwania")
+        return
 
     for i in range(tekstDlugosc):
         if i + wzorzecDlugosc > tekstDlugosc:
@@ -16,10 +17,49 @@ def algorytm_wyszukiwania(tekst,wzorzec):
             wynik = tekst[0:i] + "[" + tekst[i:i+wzorzecDlugosc] + "]"
             print(wynik)
 
+def algorytm_wyszukiwania_indeksy(tekst,wzorzec):
+    tekstDlugosc = len(tekst)
+    wzorzecDlugosc = len(wzorzec)
+    indeksy = [] #indeksy na ktorych znaleziono wzorzec
+
+    if wzorzecDlugosc > tekstDlugosc or wzorzecDlugosc == 0:
+        print("Brak mozliwosci wyszukiwania")
+        return indeksy
+
+    for i in range(tekstDlugosc-wzorzecDlugosc+1):  #ostatnia pozycja do sprawdzania to tekstDlugosc-wzorzecDlugosc
+                                                    #range konczy na jednym miejscu przed koncem dlatego +1
+        for j in range(wzorzecDlugosc):
+            if tekst[i+j] != wzorzec[j]:
+                break
+        else:
+            indeksy.append(i)
+
+    return indeksy
+
+def kolorowanie_wzorcow(tekst, wzorzec):
+    indeksy = algorytm_wyszukiwania_indeksy(tekst, wzorzec)
+    wzorzecDlugosc = len(wzorzec)
+    koniec = 0 #slaba nazwa
+    wynik = ""
+
+    ZIELONE_TLO = "\033[42m"
+    RESET = "\033[0m"
+
+    for indeks in indeksy:
+        wynik += tekst[koniec:indeks]
+        wynik += ZIELONE_TLO + tekst[indeks:indeks + wzorzecDlugosc] + RESET
+        koniec = indeks + wzorzecDlugosc
+
+    wynik += tekst[koniec:]
+    print(wynik)
+
+
+
+
 
 
 while True:
-        print("=== MENU ===")
+        print("\n=== MENU ===")
         print("Wpisz '1', aby wyszukać wzorzec w tekście")
         print("Wpisz '2', aby zakończyć program")
 
@@ -28,6 +68,8 @@ while True:
         if wybor == "1":
             print("1 dzial")
             algorytm_wyszukiwania("ala ma kota","ma")
+            print(algorytm_wyszukiwania_indeksy("ala ma kota i ma psa","ma"))
+            kolorowanie_wzorcow("ala ma kota i ma psa","ma")
 
 
         elif wybor == "2":
